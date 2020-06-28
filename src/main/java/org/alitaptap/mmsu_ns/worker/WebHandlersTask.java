83c21a11e75235a6d4fdb5fd72df46254c06873d
Jan Carlo Tolentino <tolentinojancarlo@gmail.com>
2020-06-28 21:28:46 +0800

    Improved design for collecting information about grades.

--- src/main/java/org/alitaptap/mmsu_ns/worker/WebHandlersTask.java
+++ src/main/java/org/alitaptap/mmsu_ns/worker/WebHandlersTask.java
@@ -3,6 +3,8 @@ package org.alitaptap.mmsu_ns.worker;
 import org.alitaptap.mmsu_ns.model.StudentAccount;
 import org.alitaptap.mmsu_ns.model.payload.UserPayload;
 import org.alitaptap.mmsu_ns.service.NotificationService;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.io.IOException;
 import java.util.Map;
 
+import static org.alitaptap.mmsu_ns.service.NotificationService.ResponseType.*;
+
 public class WebHandlersTask {
 
   private static final String USER_AGENT =
       "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
-
+  private final UserPayload userPayload;
   @Autowired private NotificationService notify;
-  private UserPayload userPayload;
-  private Map<String, String> cookies;
-  private String contentGrade = "";
+  private Map<String, String> authenticatedCookies;
+  private JSONArray storedGradeJson;
 
   public WebHandlersTask(UserPayload payload) {
     this.userPayload = payload;
@@ -27,6 +30,7 @@ public class WebHandlersTask {
   public void login() {
     try {
       StudentAccount studentAccount = userPayload.getStudentAccount();
+
       // Get the login page
       Connection.Response loginForm =
           Jsoup.connect("https://mys.mmsu.edu.ph/")
@@ -34,7 +38,7 @@ public class WebHandlersTask {
               .userAgent(USER_AGENT)
               .execute();
 
-      // Obtain the auth_token
+      // Obtain the auth_token for authenticating the login form
       String token =
           loginForm
               .parse()
@@ -54,8 +58,13 @@ public class WebHandlersTask {
               .method(Connection.Method.POST)
               .execute();
 
-      // Get the authenticated cookies
-      if (cookies == null) cookies = authenticateForm.cookies();
+      // Determine if login was successful, otherwise remind the user.
+      // TODO: Replace the integer value with the correct length.
+      if (authenticateForm.parse().html().length() == 357) {
+        // Get the authenticated cookies
+        if (authenticatedCookies == null) authenticatedCookies = authenticateForm.cookies();
+        notify.push(userPayload.getChatUserId(), SERVICE_LOGIN_SUCCESS);
+      } else notify.push(userPayload.getChatUserId(), SERVICE_LOGIN_FAILED);
     } catch (IOException e) {
       e.printStackTrace();
     }
@@ -69,33 +78,52 @@ public class WebHandlersTask {
           Jsoup.connect(portalUrl)
               .method(Connection.Method.GET)
               .userAgent(USER_AGENT)
-              .cookies(cookies)
+              .cookies(authenticatedCookies)
               .ignoreContentType(true)
               .execute();
 
       // Receive the initial content during execution
-      if (contentGrade.isEmpty()) contentGrade = viewGradeForm.parse().body().text();
+      if (storedGradeJson == null)
+        storedGradeJson = new JSONArray(viewGradeForm.parse().body().text());
       else {
-        // Compare new received content for every refresh
-        if (contentGrade.equals(viewGradeForm.parse().body().text())) {
-          // Parse the the JSON
+        // After getting the initial content to compare,
+        // at this point we have a new content to compare for each refresh
+        JSONArray newGradeContent = new JSONArray(viewGradeForm.parse().body().text());
 
+        // Compare new received content for every refresh
+        // Check if we have an update for the user
+        if (!storedGradeJson.similar(newGradeContent)) {
+
+          // Check if what has been updated in the grade.
+          // Identify if the grade has been POSTED, or UPLOADED.
+          // UPLOADED - When the teacher has submitted the grade,
+          //            but not yet visible to students.
+          // POSTED   - When it is approved by the chairman,
+          //            and now visible to students.
+          NotificationService.ResponseType responseType = GRADE_UPLOADED;
+
+          for (Object jsonObject : newGradeContent.toList()) {
+            JSONObject content = (JSONObject) jsonObject;
+            // TODO: Identify if grades has been posted, or uploaded.
+            System.out.println(content);
+          }
 
           // Notify the user
-          notify.gradeUpdated(userPayload.getChatUserId());
+          notify.push(userPayload.getChatUserId(), responseType);
 
-          // Set to the updated content
-          contentGrade = viewGradeForm.parse().body().text();
+          // Replace with the new updated content
+          storedGradeJson = newGradeContent;
         }
       }
-
     } catch (IOException e) {
+      notify.push(userPayload.getChatUserId(), SERVER_ERROR);
       e.printStackTrace();
     }
   }
 
   public void call() {
-    if (cookies == null) login();
-    viewGrades();
+    // Without cookies, we cannot access the grades.
+    if (authenticatedCookies == null) login();
+    else viewGrades();
   }
 }
