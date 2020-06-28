aa522033bb3013421dc15fcea753d2e87a0a0b72
Jan Carlo Tolentino <tolentinojancarlo@gmail.com>
2020-06-28 21:27:50 +0800

    Improved design for push notifications.

--- src/main/java/org/alitaptap/mmsu_ns/service/NotificationService.java
+++ src/main/java/org/alitaptap/mmsu_ns/service/NotificationService.java
@@ -14,14 +14,12 @@ public class NotificationService {
       "https://api.chatfuel.com/bots/5eede8d0a6ce1372e24625f0/users/%s/send?chatfuel_token=mELtlMAHYqR0BvgEiMq8zVek3uYUK3OJMbtyrdNPTrQB9ndV0fM7lWTFZbM4MZvD&chatfuel_message_tag=ACCOUNT_UPDATE&chatfuel_block_name=%s";
   private String PUSH_TRIGGER_API_URL = "TRIGGER_API_URL";
 
-  public void cannotLogin() {}
-
-  public void gradeUpdated(String userId) {
-    PUSH_TRIGGER_API_URL = String.format(TRIGGER_API_URL, userId, "cbot_grade_posted");
+  public void push(String chatUserId, ResponseType responseType) {
+    PUSH_TRIGGER_API_URL = String.format(TRIGGER_API_URL, chatUserId, responseType.get());
     pushNotification();
   }
 
-  public void pushNotification() {
+  private void pushNotification() {
     try {
       String USER_AGENT =
           "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36";
@@ -32,6 +30,29 @@ public class NotificationService {
           .post();
     } catch (IOException e) {
       e.printStackTrace();
+    }
+  }
+
+  public enum ResponseType {
+    GRADE_POSTED("grade_posted"),
+    GRADE_UPLOADED("grade_uploaded"),
+    SERVICE_LOGIN_SUCCESS("service_login_success"),
+    SERVICE_LOGIN_FAILED("service_login_failed"),
+    SERVICE_USER_CREATION("service_register_user"),
+    SERVICE_USER_EXISTING("service_existing_user"),
+    SERVICE_USER_MISSING("service_user_missing"),
+    SERVICE_USER_DELETION("service_unregister_user"),
+    SERVER_INTERRUPTED("server_interrupted"),
+    SERVER_ERROR("server_error");
+
+    private String responseBody;
+
+    ResponseType(String responseBody) {
+      this.responseBody = responseBody;
+    }
+
+    public String get() {
+      return responseBody;
     }
   }
 }
